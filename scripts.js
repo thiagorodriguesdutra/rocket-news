8ecdcc115d2a1f556702062a8804edebbf5dfab0
Thiago Dutra <thiagodutra@MacBook-Pro-de-Thiago.local>
2022-06-21 14:04:27 -0300

    index commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const input = document.querySelector('input');
+
+/* Simples função que exibe um alert com o valor digitado no input */
+addEventListener('submit', () => {
+  alert('O email digitado é ' + input.value)
+})
+
+/* Salvando os emails em um banco de dados */
+
+// code...
